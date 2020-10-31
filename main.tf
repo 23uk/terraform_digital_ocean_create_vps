05de18d594223f47e8045893c1b10dc995027403
eugene proshchenko <23uk@tut.by>
2020-10-31 16:02:41 +0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+data "digitalocean_ssh_key" "devops" {
+  name = "REBRAIN.SSH.PUB.KEY"
+#  name = "23uk_at_tut_by"
+}
+
+resource "digitalocean_droplet" "devops" {
+  image    = "centos-7-x64"
+  name     = "devops"
+  user_data = "REBRAIN.SSH.PUB.KEY"
+  region   = "fra1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.devops.id]
+
+}
+
